c67f5544497f437bb8a54b283406ba1e5b1b73f0
Nicolsan <nicolas.sanchezroca123@gmail.com>
2026-03-17 20:05:40 +0100

    1.1.2 struc ServerConfig

---
+++ Parsing/ServerConfig.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ServerConfig.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/17 19:49:55 by marvin            #+#    #+#             */
+/*   Updated: 2026/03/17 19:49:55 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVERCONFIG_HPP
+#define SERVERCONFIG_HPP
+
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+#include "Location.hpp"
+
+struct ServerConfig
+{
+	std::set<unsigned int> listen_ports; //ports d'ecoute
+	std::set<std::string> domain_names; //noms de domaine
+	std::string root; //chemin dossier racine
+	std::string index; //fichier index par defaut
+	std::map<int, std::string> error_pages; //code d'erreur + chemin (page 404 par exemple)
+	unsigned int client_max_body_size; //taille max du corps de la requete
+	std::vector <Location> locations; //liste des locations pour ce serveur
+};
+
+#endif 
