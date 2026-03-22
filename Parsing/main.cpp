09720096ba0c3b3557b6f8e78c63312d6b0708d8
Nicolsan <nicolas.sanchezroca123@gmail.com>
2026-03-22 15:24:44 +0100

    1.1.2 complet, mise a jour printServer, version a factoriser plus tard

--- Parsing/main.cpp
+++ Parsing/main.cpp
@@ -2,58 +2,55 @@
 #include <iostream>
 #include "ParseConfig.hpp"
 
+void print_parse_location(Location const &loc, size_t j)
+{
+    std::cout << "  - Location " << j << std::endl;
+    std::cout << "  - path: " << loc.path << std::endl;
+    std::cout << "  - root: " << loc.root << std::endl;
+    std::cout << "  - methods: ";
+    for (std::set<std::string>::const_iterator iter = loc.allowed_methods_http.begin();
+         iter != loc.allowed_methods_http.end(); ++iter)
+        std::cout << *iter << " ";
+    std::cout << std::endl;
+    std::cout << "  - index: " << loc.index << std::endl;
+    std::cout << "  - show_directory: " << loc.show_directory << std::endl;
+    std::cout << "  - upload_dir: " << loc.upload_dir << std::endl;
+    std::cout << "  - redirect_page: " << loc.redirect_page.first << " " << loc.redirect_page.second << std::endl;
+    std::cout << "  - cgi_extensions: ";
+    for (std::set<std::string>::const_iterator iter = loc.cgi_extensions.begin();
+         iter != loc.cgi_extensions.end(); ++iter)
+        std::cout << *iter << " ";
+    std::cout << std::endl;
+}
+
+void print_parse_server(ServerConfig const &server, size_t i)
+{
+    std::cout << "Server " << i << std::endl;
+    std::cout << "root " << server.root << std::endl;
+    std::cout << "domain_names ";
+    for (std::set<std::string>::const_iterator iter = server.domain_names.begin();
+         iter != server.domain_names.end(); ++iter)
+        std::cout << *iter << " ";
+    std::cout << std::endl;
+    std::cout << "ports ";
+    for (std::set<unsigned int>::const_iterator iter = server.listen_ports.begin();
+         iter != server.listen_ports.end(); ++iter)
+        std::cout << *iter << " ";
+    std::cout << std::endl;
+    std::cout << "index: " << server.index << std::endl;
+    std::cout << "client_max_body_size: " << server.client_max_body_size << std::endl;
+    for (std::map<int, std::string>::const_iterator iter = server.error_pages.begin();
+         iter != server.error_pages.end(); ++iter)
+        std::cout << "error_page: " << iter->first << " " << iter->second << std::endl;
+    for (size_t j = 0; j < server.locations.size(); j++)
+        print_parse_location(server.locations[j], j);
+}
 
 void printServers(std::vector<ServerConfig> servers)
 {
     std::cout << "nb de servers: " << servers.size() << std::endl;
     for (size_t i = 0; i < servers.size(); ++i)
-    {
-        std::cout << "Server " << i << std::endl;
-        std::cout << "root " << servers[i].root << std::endl;
-        std::cout << "domain_names ";
-        
-		for (std::set<std::string>::iterator iter = servers[i].domain_names.begin();
-            iter != servers[i].domain_names.end(); ++iter)
-            std::cout << *iter << " ";
-        std::cout << std::endl;
-        std::cout << "ports ";
-        
-		for (std::set<unsigned int>::iterator iter = servers[i].listen_ports.begin();
-        	iter != servers[i].listen_ports.end(); ++iter)
-        	std::cout << *iter << " ";
-		std::cout << std::endl;
-		std::cout << "index: " << servers[i].index << std::endl;
-		std::cout << "client_max_body_size: " << servers[i].client_max_body_size << std::endl;
-		
-		for (std::map<int, std::string>::iterator iter = servers[i].error_pages.begin();
-    		iter != servers[i].error_pages.end(); ++iter)
-    	std::cout << "error_page: " << iter->first << " " << iter->second << std::endl;
-
-        for (size_t j = 0; j < servers[i].locations.size(); j++)
-        {
-            std::cout << "  Location " << j << std::endl;
-            std::cout << "  path: " << servers[i].locations[j].path << std::endl;
-            std::cout << "  root: " << servers[i].locations[j].root << std::endl;
-            std::cout << "  methods: ";
-
-            for (std::set<std::string>::iterator iter = servers[i].locations[j].allowed_methods_http.begin();
-                iter != servers[i].locations[j].allowed_methods_http.end(); ++iter)
-                std::cout << *iter << " ";
-            std::cout << std::endl;
-
-            std::cout << "  index: " << servers[i].locations[j].index << std::endl;
-            std::cout << "  show_directory: " << servers[i].locations[j].show_directory << std::endl;
-            std::cout << "  upload_dir: " << servers[i].locations[j].upload_dir << std::endl;
-            std::cout << "  redirect_page: " << servers[i].locations[j].redirect_page.first << " " << servers[i].locations[j].redirect_page.second << std::endl;
-            std::cout << "  cgi_extensions: ";
-
-            for (std::set<std::string>::iterator iter = servers[i].locations[j].cgi_extensions.begin();
-                iter != servers[i].locations[j].cgi_extensions.end(); ++iter)
-                std::cout << *iter << " ";
-            std::cout << std::endl;
-
-        }
-    }
+        print_parse_server(servers[i], i);
 }
 
 int main(int argc, char **argv)
