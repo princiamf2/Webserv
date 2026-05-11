e50cca35067057b97b7cac088706f8662c680163
l <maelapouge@gmail.com>
2026-05-11 21:29:43 +0200

    corrected switch cases

--- srcs/HTTPRequest/RequestHandler.cpp
+++ srcs/HTTPRequest/RequestHandler.cpp
@@ -209,6 +209,7 @@ static std::string getReasonPhrase(int code)
 		case 403: return "Forbidden";
 		case 404: return "Not Found";
 		case 405: return "Method Not Allowed";
+		case 408: return "Request Timeout";
 		case 413: return "Payload Too Large";
 		case 500: return "Internal Server Error";
 		case 501: return "Not Implemented";
